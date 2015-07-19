02d117d87e2df6dadf90027b059c03201ecf9507
Joey Gauthier <digitaljedi@outlook.com>
2015-07-19 13:38:03 -0500

    added images and got weather onto navbar

--- public/modules/core/controllers/header.client.controller.js
+++ public/modules/core/controllers/header.client.controller.js
@@ -5,12 +5,16 @@ angular.module('core').controller('HeaderController', ['$scope', 'Authentication
 		$scope.authentication = Authentication;
 		$scope.isCollapsed = false;
 		$scope.currentTemp = '';
+		$scope.geoIcon = '';
+		$scope.userCity = '';
 		Geolocateme.setLocationVar();
 		$rootScope.$on('locationIsSet', function() {
 			console.log('locale', $rootScope.currentLocale);
 			Weather.getConditions($rootScope.currentLocale.latitude + ',' + $rootScope.currentLocale.longitude).then(function(data) {
+				console.log('data', data);
 				$scope.currentTemp = data.temp_f;
-				$scope.geoIcon = '/images/' + data.icon + '.png';
+				$scope.geoIcon = '/assets/images/' + data.icon + '.png';
+				$scope.userCity = data.display_location.city;
 			});
 		});
 	}
