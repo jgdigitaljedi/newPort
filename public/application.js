e07cbe86a31f0c067edf185645065552653006eb
Joey Gauthier <digitaljedi@outlook.com>
2015-07-18 21:03:23 -0500

    got angular material wired in fully

--- public/application.js
+++ public/application.js
@@ -4,9 +4,23 @@
 angular.module(ApplicationConfiguration.applicationModuleName, ApplicationConfiguration.applicationModuleVendorDependencies);
 
 // Setting HTML5 Location Mode
-angular.module(ApplicationConfiguration.applicationModuleName).config(['$locationProvider',
-	function($locationProvider) {
-		$locationProvider.hashPrefix('!');
+angular.module(ApplicationConfiguration.applicationModuleName).config(['$locationProvider',  '$mdThemingProvider', '$mdIconProvider',
+	function($locationProvider, $mdThemingProvider, $mdIconProvider) {
+		$locationProvider.html5Mode({enabled: true, requireBase: false}).hashPrefix('!');
+
+	$mdThemingProvider.theme('default')
+            .primaryPalette('blue-grey')
+            .accentPalette('deep-orange');
+ 
+        // Register the user `avatar` icons
+        $mdIconProvider
+            .defaultIconSet('./assets/svg/avatars.svg', 128)
+            .icon('menu'       , './assets/svg/menu.svg'        , 24)
+            .icon('share'      , './assets/svg/share.svg'       , 24)
+            .icon('google_plus', './assets/svg/google_plus.svg' , 512)
+            .icon('hangouts'   , './assets/svg/hangouts.svg'    , 512)
+            .icon('twitter'    , './assets/svg/twitter.svg'     , 512)
+            .icon('phone'      , './assets/svg/phone.svg'       , 512);
 	}
 ]);
 
