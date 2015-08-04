dae62bee84fb21330ed61e99e75b1b51f3e60733
Joey Gauthier <digitaljedi@outlook.com>
2015-08-03 19:29:14 -0500

    starting to wire in node mailer

--- app/routes/proxy.js
+++ app/routes/proxy.js
@@ -5,4 +5,5 @@ module.exports = function(app) {
 	var proxy = require('../../app/controllers/proxy.controller');
 	app.route('/conditions/:loc').get(proxy.conditions);
 	app.route('/lastfm').get(proxy.lastfm);
+	app.route('/contact').post(proxy.sendMail);  // Contact form route
 };
--- public/modules/core/controllers/contact-controller.client.controller.js
+++ public/modules/core/controllers/contact-controller.client.controller.js
@@ -1,7 +1,22 @@
 'use strict';
 
-angular.module('core').controller('ContactControllerController', ['$scope',
-	function($scope) {
+angular.module('core').controller('ContactControllerController', ['$scope', '$http',
+	function($scope, $http) {
+		$scope.sendEmail = function(user) {
+			console.log('first name', user.firstName);
+			console.log('last name', user.lastName);
+			console.log('email', user.email);
+			console.log('comments', user.comments);
+			$http.post('/contact', {
+				firstName: user.firstName,
+				email: user.email,
+				comment: user.comments
+			}).success(function(res) {
+
+			}).error(function(res) {
+
+			});
+		};
 		
 	}
 ]);
