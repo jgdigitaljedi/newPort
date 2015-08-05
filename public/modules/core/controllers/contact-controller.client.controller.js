03471fc84a54450a97d828817f5119ee5e2da9a8
Joey Gauthier <digitaljedi@outlook.com>
2015-08-04 20:35:52 -0500

    finished contact section with fancy alerts and icons

--- public/modules/core/controllers/contact-controller.client.controller.js
+++ public/modules/core/controllers/contact-controller.client.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
-angular.module('core').controller('ContactControllerController', ['$scope', '$http',
-	function($scope, $http) {
+angular.module('core').controller('ContactControllerController', ['$scope', '$http', '$mdDialog',
+	function($scope, $http, $mdDialog) {
 		$scope.sendEmail = function(user) {
 			console.log('first name', user.firstName);
 			console.log('last name', user.lastName);
@@ -13,11 +13,43 @@ angular.module('core').controller('ContactControllerController', ['$scope', '$ht
 				lastName: user.lastName,
 				company: user.company,
 				comment: user.comments
-			}).success(function(res) {
-
+			}).success(function(res, status) {
+				if(res.error) {
+					$mdDialog.show({
+					    controller: function DialogController($scope, $mdDialog) {
+		            		$scope.closeDialog = function() {
+		              			$mdDialog.hide();
+		            		};
+		          		},
+					    templateUrl: '/modules/core/views/failure.contact.alert.template.html',
+					    parent: angular.element(document.body)
+					});
+				} else {
+					$mdDialog.show({
+					    controller: function DialogController($scope, $mdDialog) {
+		            		$scope.closeDialog = function() {
+		              			$mdDialog.hide();
+		            		};
+		          		},
+					    templateUrl: '/modules/core/views/success.contact.alert.template.html',
+					    parent: angular.element(document.body)
+					});
+				}
 			}).error(function(res) {
-
+				$mdDialog.show({
+				    controller: function DialogController($scope, $mdDialog) {
+	            		$scope.closeDialog = function() {
+	              			$mdDialog.hide();
+	            		};
+	          		},
+				    templateUrl: '/modules/core/views/success.contact.alert.template.html',
+				    parent: angular.element(document.body)
+				});
 			});
+
+			$scope.closeDialog = function() {
+				$mdDialog.hide();
+			}
 		};
 		
 	}
