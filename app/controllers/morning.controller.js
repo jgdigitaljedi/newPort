705764680994038b6fb292750df727e1fb6ad4ab
Joey Gauthier <digitaljedi@outlook.com>
2015-10-01 05:53:44 -0500

    using json file for keys so I can commit controller logic that uses my keys

---
+++ app/controllers/morning.controller.js
@@ -0,0 +1,116 @@
+'use strict';
+
+var express = require('express'),
+    keys = require("../../keys.json"),
+    app = express(),
+    http = require('http'),
+    mornObj = {},
+    coldMessage = ['. You should take a jacket', '. It\'s gonna be a cold one', '. Chilly might be an understatement',
+        '. Turn off the infrared heater before you leave'],
+    hotMessage = ['. It will be another typical summer day in Texas', '. Triple digits once again', '. Prepare to be a sweaty mess at some point today',
+        '. I heard hell is going to be cooler than Austin today'];
+
+function randomNum(num) {
+    return Math.floor(Math.random() * num);
+}
+
+function organizeResponse(res) {
+    var currentBit = mornObj.curTemp ? '. Currently it is ' + mornObj.curTemp + ' degrees and ' + mornObj.curCondition : '. Current conditions not available',
+        forecastBit = mornObj.forecast ? '. Forecast is ' + mornObj.forecast : '. Forecast not available',
+        rainMessage = [' Better check to make sure you have an umbrella. There is a ' + mornObj.rain + ' percent chance of precipitation.',
+            ' It\'s gonna be a wet one! Looks like there is a ' + mornObj.rain + ' percent chance of precipitation.',
+            ' Expect to have soggy feet when you get to work because there is a ' + mornObj.rain + ' percent chance of precipitation.'],
+        trafficTime = mornObj.traffic ? 'Today\'s commute should take about ' +  Math.round(mornObj.traffic.travelDurationTraffic / 60) + ' minutes' : 'Bing fucked up so no traffic data';
+    if (mornObj.low <= 50) {
+        var needCoat = coldMessage[randomNum(5)];
+        currentBit += needCoat;
+    }
+    if (mornObj.high >= 100) {
+        var texasDay = hotMessage[randomNum(5)];
+        currentBit += texasDay;
+    }
+    if (parseInt(mornObj.rain) >= 30) {
+        var wetOne = rainMessage[randomNum(4)];
+        forecastBit += wetOne;
+    }
+
+    res.send(trafficTime + currentBit + forecastBit);
+}
+
+function getCalendar(res) {
+    organizeResponse(res);
+}
+
+function getForecast(res) {
+    http.get({
+        host: 'api.wunderground.com',
+        path: '/api/' + keys.wu + '/forecast/q/TX/Austin.json'
+    }, function (response) {
+        var body = '';
+        if (response.statusCode < 200 || response.statusCode > 299) {
+            mornObj.forecast = false;
+        } else {
+            response.on('data', function (d) {
+                body += d;
+            });
+            response.on('end', function () {
+                var parsed = JSON.parse(body);
+                mornObj.forecast = parsed.forecast.txt_forecast.forecastday[0].fcttext;
+                mornObj.rain = parsed.forecast.txt_forecast.forecastday[0]['pop'];
+                mornObj.high = Math.round(parsed.forecast.simpleforecast.forecastday[0].high.fahrenheit);
+                mornObj.low = Math.round(parsed.forecast.simpleforecast.forecastday[0].low.fahrenheit);
+                getCalendar(res);
+            });
+        }
+    });
+}
+
+function getConditions(res) {
+    http.get({
+        host: 'api.wunderground.com',
+        path: '/api/' + keys.wu + '/conditions/q/TX/Austin.json'
+    }, function (response) {
+        var body = '';
+        if (response.statusCode < 200 || response.statusCode > 299) {
+            mornObj.curTemp = false;
+        } else {
+            response.on('data', function (d) {
+                body += d;
+            });
+            response.on('end', function () {
+                var parsed = JSON.parse(body);
+                mornObj.curTemp = Math.round(parsed.current_observation.temp_f);
+                mornObj.curCondition = parsed.current_observation.weather;
+                mornObj.precip = parsed.current_observation.precip_today_in;
+                getForecast(res);
+            });
+        }
+    });
+}
+
+function getTraffic(res) {
+    var home = encodeURI(keys.homeAdd),
+        parking = encodeURI(keys.workAdd);
+    http.get({
+        host: 'dev.virtualearth.net',
+        path: '/REST/V1/Routes/Driving?wp.0=' + home + '&wp.1=' + parking + '&avoid=minimizeTolls&key=' + keys.bing
+    }, function (response) {
+        var body = '';
+        if (response.statusCode < 200 || response.statusCode > 299) {
+            mornObj.traffic = false;
+        } else {
+            response.on('data', function (d) {
+                body += d;
+            });
+            response.on('end', function () {
+                var parsed = JSON.parse(body);
+                mornObj.traffic = parsed.resourceSets[0].resources[0];
+                getConditions(res);
+            });
+        }
+    });
+}
+
+exports.getInfo = function(req, res) {
+    getTraffic(res);
+}
