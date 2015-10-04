6641a36ad3235215477c9a4064fe8d9198bd55f7
Joey Gauthier <digitaljedi@outlook.com>
2015-10-04 15:30:01 -0500

    resume section done, ready to launch

--- public/modules/core/controllers/resume-controller.client.controller.js
+++ public/modules/core/controllers/resume-controller.client.controller.js
@@ -1,8 +1,15 @@
 'use strict';
 
-angular.module('core').controller('ResumeControllerController', ['$scope',
-	function($scope) {
-		// Resume controller controller logic
-		// ...
+angular.module('core').controller('ResumeControllerController', ['$scope', '$window',
+	function($scope, $window) {
+		$scope.openFab = false;
+
+		$scope.docClicked = function () {
+            $window.open('../../assets/resume/Joey_Gauthier_resume_word.docx', '_blank');
+		};
+
+		$scope.pdfClicked = function () {
+			$window.open('../../assets/resume/Joey_Gauthier_resume_compressed.pdf', '_blank');
+		};
 	}
 ]);
