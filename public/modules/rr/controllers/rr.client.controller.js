5e97ff8fb3498419e56630dade403643560e6c76
Joey Gauthier <digitaljedi@outlook.com>
2016-02-28 09:32:01 -0600

    restaurant roulette splash screen started and simple service started

--- public/modules/rr/controllers/rr.client.controller.js
+++ public/modules/rr/controllers/rr.client.controller.js
@@ -1,18 +1,17 @@
 'use strict';
 
 
-angular.module('rr').controller('RrController', ['$scope', 'Authentication', '$mdSidenav', 'Yelp', '$rootScope', 'Geolocateme',
-	function($scope, Authentication, $mdSidenav, Yelp, $rootScope, Geolocateme) {
-		// This provides Authentication context which I'll probably never use but it's here in case I do.
-        // TODO: consider making sidenav for mobile view instead of tabs
-		$scope.authentication = Authentication;
+angular.module('rr').controller('RrController', ['$scope', 'Authentication', '$mdSidenav', 'Yelp', '$rootScope', 'Geolocateme', 'Yelpers',
+	function($scope, Authentication, $mdSidenav, Yelp, $rootScope, Geolocateme, Yelpers) {
+		var rrVm = this;
+
 		function callYelp () {
 			var request = {lat: $rootScope.currentLocale.latitude, long: $rootScope.currentLocale.longitude};
 			Yelp.getYelpInfo(request.lat, request.long).then(function (response) {
 				console.log('yelp response for future features', response);
 			});
 		}
-		if(!sessionStorage.getItem('geoLocation')) {
+		if (!sessionStorage.getItem('geoLocation')) {
 			Geolocateme.setLocationVar();
 		} else {
 			$rootScope.currentLocale = JSON.parse(sessionStorage.getItem('geoLocation'));
@@ -21,19 +20,6 @@ angular.module('rr').controller('RrController', ['$scope', 'Authentication', '$m
 		$rootScope.$on('locationIsSet', function () {
 			callYelp();
 		});
-
-		function toggleUsersList() {
-            $mdSidenav('left').toggle();
-        }
-        //$scope.makeRoute = function (which) {
-        //    //$location.path() = '/' + which;
-        //};
-
-        var self = this;
-        
-        self.selected     = null;
-        self.users        = [ ];
-        self.toggleList   = toggleUsersList;
 	}
 
 ]);
