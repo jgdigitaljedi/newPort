aabd2454a5c567eeb79f6aa3849e5fcb48ff0109
Joey Gauthier <digitaljedi@outlook.com>
2016-02-28 20:03:09 -0600

    droplet ram not enough to build with uglify

--- gruntfile.js
+++ gruntfile.js
@@ -235,7 +235,8 @@ module.exports = function(grunt) {
 	grunt.registerTask('lint', ['jshint', 'less', 'csslint']);
 
 	// Build task(s).
-	grunt.registerTask('build', ['loadConfig', 'ngAnnotate', 'concat', 'uglify', 'less', 'postcss', 'cssmin']);
+	// grunt.registerTask('build', ['loadConfig', 'ngAnnotate', 'concat', 'uglify', 'less', 'postcss', 'cssmin']);
+	grunt.registerTask('build', ['loadConfig', 'ngAnnotate', 'concat', 'less', 'postcss', 'cssmin']);
 
 	// Test task.
 	grunt.registerTask('test', ['env:test', 'mochaTest', 'karma:unit']);
